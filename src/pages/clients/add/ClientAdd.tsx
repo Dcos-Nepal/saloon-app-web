c4762f6dea7028761e5fd73d6d1ebec5d65db7b7
susankoju <aksusankoju@gmail.com>
2022-01-29 08:28:20 +0545

    OCA: Add client phone number add/remove and design update

--- src/pages/clients/add/ClientAdd.tsx
+++ src/pages/clients/add/ClientAdd.tsx
@@ -1,6 +1,5 @@
 import { useNavigate } from "react-router-dom";
 import ClientDetailForm from "./ClientDetailForm";
-import PropertyDetail from "./PropertyDetail";
 
 const ClientAdd = () => {
   const navigate = useNavigate();
@@ -8,10 +7,7 @@ const ClientAdd = () => {
   return (
     <>
       <div className="row">
-        <div
-          className="txt-orange pointer"
-          onClick={() => navigate(-1)}
-        >
+        <div className="txt-orange pointer" onClick={() => navigate(-1)}>
           <span className="col me-1">
             <box-icon name="arrow-back" size="xs" color="#EC7100" />
           </span>
@@ -21,16 +17,7 @@ const ClientAdd = () => {
           <h3>New Client</h3>
         </div>
       </div>
-      <div className="row m-1">
-        <div className="col card">
-          <h5>Client Details</h5>
-          <ClientDetailForm />
-        </div>
-        <div className="col card ms-3">
-          <h5>Property Details</h5>
-          <PropertyDetail />
-        </div>
-      </div>
+      <ClientDetailForm />
     </>
   );
 };

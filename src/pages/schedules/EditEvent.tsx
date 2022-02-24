4d7f2d1a10ced3c99f35c3cd6f18cb3b7b20e1e2
Saroj Maharjan <sarojmaharjan@lftechnology.com>
2022-02-24 20:48:21 +0545

    edit visit feature added

--- src/pages/schedules/EditEvent.tsx
+++ src/pages/schedules/EditEvent.tsx
@@ -1,5 +1,5 @@
-import { FC } from "react";
-import { useNavigate } from "react-router-dom";
+import { FC } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 interface IProps {
   event: any;
@@ -41,11 +41,15 @@ const EditEvent: FC<IProps> = ({ closeModal, event }) => {
           <h5>Details</h5>
           <div>{event.title}</div>
           <div className="mt-3">
-            <label><strong>Visit Status</strong></label>
+            <label>
+              <strong>Visit Status</strong>
+            </label>
             <div className="mt-1">
-              {event.extendedProps?.meta?.status?.status === 'COMPLETED'
-                ? <span className="status status-green">{event.extendedProps?.meta?.status?.status}</span>
-                : <span className="status status-blue">{event.extendedProps?.meta?.status?.status}</span>}
+              {event.extendedProps?.meta?.status?.status === 'COMPLETED' ? (
+                <span className="status status-green">{event.extendedProps?.meta?.status?.status}</span>
+              ) : (
+                <span className="status status-blue">{event.extendedProps?.meta?.status?.status}</span>
+              )}
             </div>
           </div>
         </div>
@@ -55,10 +59,15 @@ const EditEvent: FC<IProps> = ({ closeModal, event }) => {
         <div className="row">
           <h5>Team</h5>
           <div>
-            {event.extendedProps?.meta?.job?.team
-              ? event.extendedProps?.meta?.job?.team.map((mem: { fullName: string }) => (<><span className="status status-blue">{mem.fullName}</span> &nbsp;</>))
-              : <div>No Team Assigned</div>
-            }
+            {event.extendedProps?.meta?.job?.team ? (
+              event.extendedProps?.meta?.job?.team.map((mem: { fullName: string }) => (
+                <>
+                  <span className="status status-blue">{mem.fullName}</span> &nbsp;
+                </>
+              ))
+            ) : (
+              <div>No Team Assigned</div>
+            )}
           </div>
         </div>
 
@@ -66,37 +75,38 @@ const EditEvent: FC<IProps> = ({ closeModal, event }) => {
 
         <div className="row">
           <h5>Property Location</h5>
-          <div className="txt-grey">{event.extendedProps?.meta?.job?.property.name}</div>
+          <div className="txt-grey">{event.extendedProps?.meta?.job?.property?.name || ''}</div>
           <div className="">
-            {event.extendedProps?.meta?.job?.property?.street1}, {event.extendedProps?.meta?.job?.property?.postalCode}, {event.extendedProps?.meta?.job?.property?.city}, {event.extendedProps?.meta?.job?.property?.state}, {event.extendedProps?.meta?.job?.property?.country}
+            {event.extendedProps?.meta?.job?.property?.street1}, {event.extendedProps?.meta?.job?.property?.postalCode},{' '}
+            {event.extendedProps?.meta?.job?.property?.city}, {event.extendedProps?.meta?.job?.property?.state},{' '}
+            {event.extendedProps?.meta?.job?.property?.country}
           </div>
         </div>
 
         <div className="hr mb-3"></div>
 
         <div className="row">
-          {event.start ? (<div className="col">
-            <h5>Start Date/Time</h5>
-            <div>{new Date(event.start).toLocaleDateString()}</div>
-            <div>{new Date(event.start).toLocaleTimeString()}</div>
-          </div>) : null}
-          {event.end ? (<div className="col">
-            <h5>End Date/Time</h5>
-            <div>{new Date(event.end).toLocaleDateString()}</div>
-            <div>{new Date(event.end).toLocaleTimeString()}</div>
-          </div>) : null}
-          
+          {event.start ? (
+            <div className="col">
+              <h5>Start Date/Time</h5>
+              <div>{new Date(event.start).toLocaleDateString()}</div>
+              <div>{new Date(event.start).toLocaleTimeString()}</div>
+            </div>
+          ) : null}
+          {event.end ? (
+            <div className="col">
+              <h5>End Date/Time</h5>
+              <div>{new Date(event.end).toLocaleDateString()}</div>
+              <div>{new Date(event.end).toLocaleTimeString()}</div>
+            </div>
+          ) : null}
         </div>
       </div>
       <div className="modal-footer">
         <button onClick={() => navigate('/dashboard/quotes/123456')} type="button" className="btn btn-primary">
           View details
         </button>
-        <button
-          onClick={closeModal}
-          type="button"
-          className="btn btn-secondary"
-        >
+        <button onClick={closeModal} type="button" className="btn btn-secondary">
           Edit
         </button>
       </div>

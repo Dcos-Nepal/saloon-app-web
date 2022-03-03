dec7d944f4cfbfdce1d98ca302b6a452dfdf1f57
susankoju <aksusankoju@gmail.com>
2022-03-03 19:10:18 +0545

    OCA: Add client detail active jobs, invoices

--- src/pages/clients/detail/ClientDetail.tsx
+++ src/pages/clients/detail/ClientDetail.tsx
@@ -3,14 +3,15 @@ import pinterpolate from 'pinterpolate';
 import { FC, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
+import { getCurrentUser } from 'utils';
 import { IClient } from 'common/types/client';
 import { Loader } from 'common/components/atoms/Loader';
 import * as jobsActions from 'store/actions/job.actions';
 import * as quotesActions from 'store/actions/quotes.actions';
 import * as clientsActions from 'store/actions/clients.actions';
+import * as invoicesActions from 'store/actions/invoices.actions';
 import * as jobReqActions from 'store/actions/job-requests.actions';
 import * as propertiesActions from 'store/actions/properties.actions';
-import { getCurrentUser } from 'utils';
 
 interface IRequest {
   id: string;
@@ -34,11 +35,29 @@ interface IQuote {
   updatedAt: string;
 }
 
+interface IInvoice {
+  id: string;
+  subject: string;
+  clientMessage: string;
+  dueOnReceipt: boolean;
+  isPaid: boolean;
+  isIssued: boolean;
+  invoiceFor: any;
+  refJob?: any;
+  refVisit?: any;
+  refProperty?: any;
+  lineItems: any[];
+  total: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
 interface IProps {
   actions: {
     fetchJobs: (query: any) => any;
     fetchQuotes: (query: any) => any;
     fetchClient: (id: string) => void;
+    fetchInvoices: (filter: any) => void;
     fetchJobRequests: (query: any) => any;
     updateClient: (data: IClient) => void;
     fetchProperties: (filter: any) => void;
@@ -47,32 +66,33 @@ interface IProps {
   jobs: any[];
   quotes: IQuote[];
   properties: any[];
+  invoices: IInvoice[];
   requests: IRequest[];
   isClientsLoading: boolean;
   currentClient: IClient;
 }
 
-const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, requests, jobs }) => {
+const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, requests, invoices, jobs }) => {
   const { id } = useParams();
   const navigate = useNavigate();
   const currentUser = getCurrentUser();
 
   const Tabs = {
-    ActiveWork: 'ActiveWork',
+    ActiveJob: 'ActiveJob',
     Requests: 'Requests',
     Quotes: 'Quotes',
     Jobs: 'Jobs',
     Invoices: 'Invoices'
   };
 
-  const [tab, setTab] = useState(Tabs.ActiveWork);
+  const [tab, setTab] = useState(Tabs.ActiveJob);
 
   useEffect(() => {
     if (id) actions.fetchClient(id);
   }, [id, actions]);
 
   useEffect(() => {
-    const jobQuery: { createdBy?: string; team?: string; jobFor?: string; } = {};
+    const jobQuery: { createdBy?: string; team?: string; jobFor?: string } = {};
 
     if (currentUser.role === 'WORKER') {
       jobQuery.team = currentUser.id;
@@ -83,8 +103,9 @@ const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, 
     if (currentUser.role !== 'WORKER') {
       actions.fetchQuotes({ quoteFor: id });
       actions.fetchJobRequests({ client: id });
+      actions.fetchInvoices({ invoiceFor: id });
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [id, actions]);
 
   useEffect(() => {
@@ -153,12 +174,56 @@ const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, 
     );
   };
 
-  const Jobs = () => {
+  const Invoices = () => {
     return (
       <div className="row mt-4">
-        {jobs.length ? (
+        {invoices.length ? (
           <>
-            {jobs.map((job: any) => (
+            {invoices.map((invoice: IInvoice) => (
+              <div
+                onClick={() => navigate(pinterpolate('/dashboard/invoices/:id', { id: invoice.id }))}
+                key={invoice.id}
+                className="pointer hover-grey row mb-3 border-bottom"
+              >
+                <div className="col p-2 ps-4">
+                  <div className="txt-grey">{invoice.subject}</div>
+                  <div className="">{invoice.clientMessage}</div>
+                </div>
+                <div className="col p-2 ps-4">
+                  {invoice.isPaid ? (
+                    <span className="d-flex status status-green float-end">Paid</span>
+                  ) : (
+                    <span className="d-flex status status-blue float-end">Pending</span>
+                  )}
+                </div>
+              </div>
+            ))}
+          </>
+        ) : (
+          <div className="col p-2 ps-4">
+            <div className="txt-grey">No invoices</div>
+            <div className="">No active jobs, invoices or quotes for this client yet</div>
+          </div>
+        )}
+      </div>
+    );
+  };
+
+  const Jobs = ({ activeOnly = false }) => {
+    let activeTabJobs = jobs;
+    if (activeOnly) {
+      const nowTime = new Date().getTime();
+
+      activeTabJobs = jobs.filter((job) => {
+        return !job.isCompleted && job.startDate && new Date(job.startDate).getTime() < nowTime;
+      });
+    }
+
+    return (
+      <div className="row mt-4">
+        {activeTabJobs.length ? (
+          <>
+            {activeTabJobs.map((job: any) => (
               <div
                 onClick={() => navigate(pinterpolate('/dashboard/jobs/:id', { id: job.id }))}
                 key={job.id}
@@ -186,24 +251,10 @@ const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, 
 
   const TabContent = () => {
     switch (tab) {
-      case Tabs.ActiveWork:
-        return (
-          <div className="row mt-4">
-            <div className="col p-2 ps-4">
-              <div className="txt-grey">No active work</div>
-              <div className="">No active jobs, invoices or quotes for this client yet</div>
-            </div>
-          </div>
-        );
+      case Tabs.ActiveJob:
+        return <Jobs activeOnly={true} />;
       case Tabs.Invoices:
-        return (
-          <div className="row mt-4">
-            <div className="col p-2 ps-4">
-              <div className="txt-grey">No invoices</div>
-              <div className="">No active jobs, invoices or quotes for this client yet</div>
-            </div>
-          </div>
-        );
+        return <Invoices />;
       case Tabs.Jobs:
         return <Jobs />;
       case Tabs.Quotes:
@@ -237,11 +288,7 @@ const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, 
             <div className="d-flex flex-row mt-2">
               <h3 className="txt-bold extra">{currentClient.fullName || `${currentClient.firstName} ${currentClient.lastName}`}</h3>
               <div className="col">
-                <button
-                  onClick={() => id && navigate(`edit`)}
-                  type="button"
-                  className="btn btn-primary d-flex float-end me-2"
-                >
+                <button onClick={() => id && navigate(`edit`)} type="button" className="btn btn-primary d-flex float-end me-2">
                   Edit Client
                 </button>
               </div>
@@ -312,8 +359,12 @@ const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, 
                 <div className="row mt-2">
                   <div className="col p-2 ps-4">
                     <div className="txt-grey">Client's Address</div>
-                    <div className="">{currentClient.address?.street1}, {currentClient.address?.street2}, {currentClient.address?.postalCode}</div>
-                    <div className=''>{currentClient.address?.city}, {currentClient.address?.state}, {currentClient.address?.country}</div>
+                    <div className="">
+                      {currentClient.address?.street1}, {currentClient.address?.street2}, {currentClient.address?.postalCode}
+                    </div>
+                    <div className="">
+                      {currentClient.address?.city}, {currentClient.address?.state}, {currentClient.address?.country}
+                    </div>
                   </div>
                 </div>
               </div>
@@ -326,10 +377,10 @@ const ClientDetail: FC<IProps> = ({ actions, currentClient, quotes, properties, 
               </div>
               <div className="">
                 <div className="row mt-3">
-                  <div className={`col tab me-1 ${tab === Tabs.ActiveWork ? 'active-tab' : ''}`} onClick={() => setTab(Tabs.ActiveWork)}>
-                    Active work
+                  <div className={`col tab me-1 ${tab === Tabs.ActiveJob ? 'active-tab' : ''}`} onClick={() => setTab(Tabs.ActiveJob)}>
+                    Active jobs
                   </div>
-                  {(currentUser.role !== 'WORKER') ? (
+                  {currentUser.role !== 'WORKER' ? (
                     <>
                       <div className={`col tab me-1 ${tab === Tabs.Requests ? 'active-tab' : ''}`} onClick={() => setTab(Tabs.Requests)}>
                         Requests
@@ -365,6 +416,7 @@ const mapStateToProps = (state: any) => {
     jobs: state.jobs.jobs?.data?.rows || [],
     properties: state.properties.properties || [],
     quotes: state.quotes.itemList?.data?.rows || [],
+    invoices: state.invoices.itemList?.data?.rows || [],
     requests: state.jobRequests.itemList?.data?.rows || []
   };
 };
@@ -388,6 +440,9 @@ const mapDispatchToProps = (dispatch: any) => ({
     },
     fetchClient: (id: string) => {
       dispatch(clientsActions.fetchClient(id));
+    },
+    fetchInvoices: (payload: any) => {
+      dispatch(invoicesActions.fetchInvoices(payload));
     },
     updateClient: (data: IClient) => {
       dispatch(clientsActions.updateClient(data));

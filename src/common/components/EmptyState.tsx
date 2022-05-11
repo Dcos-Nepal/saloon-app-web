136a269d27b30bc284f8f8a46d1b48ef13571cd5
Buddha Man Nepali <bmnepali980@gmail.com>
2022-05-11 12:27:21 +0545

    OCA: Adds Empty state in all list views

--- src/common/components/EmptyState.tsx
+++ src/common/components/EmptyState.tsx
@@ -5,7 +5,7 @@ const EmptyState = () => {
     <div className='blank-state blank-state-sm'>
       <LockIcon verticalAlign="middle" size={'medium'}/>
       <h6 className='mt-2'>All Caught Up!</h6>
-      <p>Your search query does not match any results.</p>
+      <div>Your search query does not match any results.</div>
       <div>Please reload this page</div>
 
     </div>

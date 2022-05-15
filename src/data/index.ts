c515340e5d76306065286414cb3b0515d529a703
Buddha Man Nepali <bmnepali980@gmail.com>
2022-05-15 14:45:34 +0545

    OCA: Adds more services

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,7 @@
 export const getServices = () => {
   return [
+    { label: 'Domestic Clean', value: 'domestic-clean', isActive: true },
+    { label: 'Office Clean', value: 'domestic-clean', isActive: true },
     { label: 'Regular Clean', value: 'regular-clean', isActive: true },
     { label: 'Once off Clean', value: 'once-off-clean', isActive: true },
     { label: 'Presell Clean', value: 'presell-clean', isActive: true },

e92e611a5c390ef1e3f444183a18338d33d8a9a3
Buddha Man Nepali <bmnepali980@gmail.com>
2022-06-21 22:25:32 +0545

    OCA: Adds two pages

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import LogoFull from "assets/images/LogoFull.svg";
+import { endpoints } from 'common/config';
+
+const PageNotFound = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="container-fluid txt-grey">
+      <div className="vh-100 d-flex justify-content-center align-items-center">
+        <div className="mt-5">
+          <div className="d-flex justify-content-center mb-4">
+            <img src={LogoFull} alt="Orange Cleaning" style={{height: '145px'}} />
+          </div>
+          <div className="main-container card bg-white p-4">
+            <div>
+              <h4>404 Page Not Found!</h4>
+              <label>The page you are looking for does not exist.</label>
+            </div>
+            <div className="d-flex justify-content-center mt-2">
+              <button type="button" className="btn btn-primary btn-full" onClick={() => navigate(endpoints.admin.home)}>Reload Now</button>
+            </div>
+          </div>
+          <div className='mb-5 mt-5 text-center pb-5'>
+            Copyright &copy; {new Date().getFullYear()} <b>Orange Cleaning</b>, All Rights Reserved.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PageNotFound;
